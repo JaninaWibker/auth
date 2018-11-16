e27956ee5865fe3482a00ed8dc173d03186b4197
Jannik Wibker <jannikwibker@gmail.com>
2018-11-16 19:57:48 +0100

    added api support for invalidating register tokens

--- index.js
+++ index.js
@@ -179,6 +179,27 @@ app.post('/validate-register-token', passport.authenticate('jwt', { session: fal
   res.json(validateRegisterToken(req.body.register_token))
 })
 
+app.post('/invalidate-register-token', passport.authenticate('jwt', { session: false }), (req, res) => {
+  db.getUserFromIdIfExists(req.user.id, (err, user, info) => {
+    if(err) res.status(500).json({ message: 'failed to validate user' })
+    if(user.account_type === 'admin') {
+      if(req.body.id || req.body.register_token) {
+        if(req.body.id) {
+          registerTokenCache.del(req.body.id)
+          res.json({ message: 'register token invalidated successfully' })
+        } else {
+          registerTokenCache.del(validateRegisterToken(req.body.register_token).id)
+          res.json({ message: 'register token invalidated successfully' })
+        }
+      } else {
+        res.status(500).json({ message: 'supply register token id or token' })
+      }
+    } else {
+      res.status(403).json({ message: 'account not permitted' })
+    }
+  })
+})
+
 app.post(['/add', '/users/add'], (req, res) => {
   const data = req.body
   if(data.username && data.password && data.first_name && data.last_name && data.email) {

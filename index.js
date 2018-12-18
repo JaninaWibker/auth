00798cbdb3e95dab956bd0b86d02f199419dfb65
Jannik Wibker <jannikwibker@gmail.com>
2018-12-18 14:16:46 +0100

    better logging

--- index.js
+++ index.js
@@ -88,14 +88,22 @@ app.use(passport.initialize())
 app.get('/', (_, res) => res.send('server is up and running'))
 
 app.post(['/register', '/service/register'], (req, res) => {
+  console.log('[' + format_date() + '][service/register] "' + req.body.data.name + '" trying to register...')
   if(validateService(Buffer.from(req.get('Authorization').substr('Bearer '.length), 'base64'), {...req.body.data, timestamp: req.body.timestamp})) {
     if(serviceCache.keys().includes(req.body.data.name)) {
+      console.log('[' + format_date() + '][service/register] "' + req.body.data.name + '" was already a known service')
       res.json({ message: 'already registered', public_key: public_key })
     } else {
       serviceCache.put(req.body.data.name, req.body.data)
+      console.log('[' + format_date() + '][service/register] "' + req.body.data.name + '" registered successfully')
       res.json({ message: 'registration successful', public_key: public_key })
     }
   } else {
+    console.log(
+      '[' + format_date() + '][service/register] "' + req.body.data.name + '" failed to register...\nvalidation failed, investigation may be required', 
+      'BODY', {...req.body.data, timestamp: req.body.timestamp, }, 
+      'HEADER', req.get('Authorization')
+    )
     res.json({ message: 'registration failed', public_key: public_key })
   }
 })

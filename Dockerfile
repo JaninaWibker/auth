5157d6c2e8223e133677a48e92f8a245c1e0e14f
Jannik Wibker <jannikwibker@gmail.com>
2019-06-25 14:08:26 +0200

    "fixed" cors errors, updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM node:11
 
 # package.json version
 LABEL version="1.4.1"
+LABEL name="auth"
 
 ARG RESET_DATABASES
 ARG DB="postgres"
 
+
+# complicated install process for sqlite3 and node-sqlite3 since no pre-build version of node-sqlite3 is available for ARM
 RUN if [ "$DB" = "sqlite" ] ; then apt-get update ; fi
 RUN if [ "$DB" = "sqlite" ] ; then apt-get -yq install sqlite3 libsqlite3-dev ; fi
 
@@ -18,5 +21,7 @@ RUN if [ "$DB" = "postgres" ] ; then npm install ; fi
 
 # resetting databases (created if they don't already exist) if RESET_DATABASES is set to true
 RUN if [ "$RESET_DATABASES" = "true" ] ; then node tasks/setup.js ; fi
+
+# the presence of a correctly configured .env file is assumed
 
 CMD node index.js
--- utils/cors.js
+++ utils/cors.js
@@ -8,13 +8,17 @@ const cors = (req, res, next) => {
   const url = URL.parse(req.protocol + '://' + req.get('host'))
   const url_reversed_arr = url.hostname.split('.').reverse()
 
-  if((url_reversed_arr[0] === 'ml' && url_reversed_arr[1] === 'jannik') 
-  || (url_reversed_arr[0] === 'net' && url_reversed_arr[1] === 'ddns' && url_reversed_arr[2] === 'jannik')
-  || (url_reversed_arr[3] === '192' && url_reversed_arr[2] === '168' && url_reversed_arr[1] === '178')
-  || (url_reversed_arr[0] === 'jannik-rpi3')
-  || (url_reversed_arr[0] === 'jannik-mbp-2017')
-  || (url_reversed_arr[0] === 'samba-server')
-  || (url_reversed_arr[0] === 'localhost')) {
+  // if((url_reversed_arr[0] === 'ml' && url_reversed_arr[1] === 'jannik') 
+  // || (url_reversed_arr[0] === 'net' && url_reversed_arr[1] === 'ddns' && url_reversed_arr[2] === 'jannik')
+  // || (url_reversed_arr[3] === '192' && url_reversed_arr[2] === '168' && url_reversed_arr[1] === '178')
+  // || (url_reversed_arr[0] === 'jannik-rpi3')
+  // || (url_reversed_arr[0] === 'jannik-mbp-2017')
+  // || (url_reversed_arr[0] === 'samba-server')
+  // || (url_reversed_arr[0] === 'localhost')
+  // || (url_reversed_arr[0] === 'auth-server')
+  // || (url_reversed_arr[0] === 'analytics')
+  // || (url_reversed_arr[0] === 'notes-backend')) {
+  if(true) {
     res.header('Access-Control-Allow-Origin', req.get('origin'))
   } else {
     console.log('[CORS] Unauthorized Access from ' + req.get('host'))
